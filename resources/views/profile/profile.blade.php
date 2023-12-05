86fe62c77454531254442ef4b851e4def40b6672
divyeshz <divyeshm@zignuts.com>
2023-12-05 16:15:13 +0530

    Improve user profile information display

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -43,23 +43,20 @@
                         <div
                             class="d-flex align-items-md-end align-items-sm-start align-items-center justify-content-md-between justify-content-start mx-4 flex-md-row flex-column gap-4">
                             <div class="user-profile-info">
-                                <h4>John Doe</h4>
+                                <h4>{{ Auth::user()->name }}</h4>
                                 <ul
                                     class="list-inline mb-0 d-flex align-items-center flex-wrap justify-content-sm-start justify-content-center gap-2">
                                     <li class="list-inline-item fw-medium">
-                                        <i class='bx bx-pen'></i> UX Designer
+                                        <i class='bx bx-pen'></i> Web Developer
                                     </li>
                                     <li class="list-inline-item fw-medium">
-                                        <i class='bx bx-map'></i> Vatican City
+                                        <i class='bx bx-map'></i> India
                                     </li>
                                     <li class="list-inline-item fw-medium">
-                                        <i class='bx bx-calendar-alt'></i> Joined April 2021
+                                        <i class='bx bx-calendar-alt'></i> Joined April 2023
                                     </li>
                                 </ul>
                             </div>
-                            <a href="javascript:void(0)" class="btn btn-primary text-nowrap">
-                                <i class='bx bx-user-check me-1'></i>Connected
-                            </a>
                         </div>
                     </div>
                 </div>
@@ -68,44 +65,33 @@
     </div>
     <!--/ Header -->
 
-    <!-- Navbar pills -->
-    <div class="row">
-        <div class="col-md-12">
-            <ul class="nav nav-pills flex-column flex-sm-row mb-4">
-                <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i class='bx bx-user me-1'></i>
-                        Profile</a></li>
-            </ul>
-        </div>
-    </div>
-    <!--/ Navbar pills -->
-
     <!-- User Profile Content -->
     <div class="row">
-        <div class="col-xl-4 col-lg-5 col-md-5">
+        <div class="col-12">
             <!-- About User -->
             <div class="card mb-4">
                 <div class="card-body">
                     <small class="text-muted text-uppercase">About</small>
                     <ul class="list-unstyled mb-4 mt-3">
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-user"></i><span
-                                class="fw-medium mx-2">Full Name:</span> <span>John Doe</span></li>
+                                class="fw-medium mx-2">Full Name:</span> <span>{{ Auth::user()->name }}</span></li>
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-check"></i><span
-                                class="fw-medium mx-2">Status:</span> <span>Active</span></li>
+                                class="fw-medium mx-2">Status:</span> <span>{{ Auth::user()->is_active == true ? "Active" : "Inactive" }} </span></li>
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-star"></i><span
                                 class="fw-medium mx-2">Role:</span> <span>Developer</span></li>
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-flag"></i><span
-                                class="fw-medium mx-2">Country:</span> <span>USA</span></li>
+                                class="fw-medium mx-2">Country:</span> <span>India</span></li>
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-detail"></i><span
                                 class="fw-medium mx-2">Languages:</span> <span>English</span></li>
                     </ul>
                     <small class="text-muted text-uppercase">Contacts</small>
                     <ul class="list-unstyled mb-4 mt-3">
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-phone"></i><span
-                                class="fw-medium mx-2">Contact:</span> <span>(123) 456-7890</span></li>
+                                class="fw-medium mx-2">Contact:</span> <span>{{ Auth::user()->phone ?? '-' }}</span></li>
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-chat"></i><span
                                 class="fw-medium mx-2">Skype:</span> <span>john.doe</span></li>
                         <li class="d-flex align-items-center mb-3"><i class="bx bx-envelope"></i><span
-                                class="fw-medium mx-2">Email:</span> <span>john.doe@example.com</span></li>
+                                class="fw-medium mx-2">Email:</span> <span>{{ Auth::user()->email }}</span></li>
                     </ul>
                 </div>
             </div>
