b529aacd479666a34a903c3d7423af9c67df8194
divyeshz <divyeshm@zignuts.com>
2023-12-08 10:24:10 +0530

    Add BaseModel and update Post model to implement common functionality

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class BaseModel extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+
+    protected static function bootMethod()
+    {
+        static::creating(function ($module) {
+            $module->id = Str::uuid()->toString();
+            $userId = auth()->id() ?? null;
+            $module->created_by = $userId;
+        });
+
+        static::updating(function ($module) {
+            $userId = auth()->id() ?? null;
+            $module->updated_by = $userId;
+        });
+
+        static::deleting(function ($module) {
+            $userId = auth()->id() ?? null;
+            $module->deleted_by = $userId;
+            $module->is_deleted = 1;
+            $module->save();
+        });
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Post extends Model
+class Post  extends BaseModel
 {
   use HasFactory;
 
@@ -26,26 +26,13 @@ class Post extends Model
 
   protected static function booted()
   {
-    static::creating(function ($post) {
-      $post->id = Str::uuid()->toString();
-      $userId = auth()->id() ?? null;
-      $post->created_by = $userId;
-    });
-
-    static::updating(function ($post) {
-      $userId = auth()->id() ?? null;
-      $post->updated_by = $userId;
-    });
-
-    static::deleting(function ($post) {
-      $userId = auth()->id() ?? null;
-      $post->deleted_by = $userId;
-    });
+    parent::booted();
+    static::bootMethod();
   }
 
   // Define the relationship with users through the shared_posts table
   public function users()
   {
-      return $this->belongsToMany(User::class, 'shared_posts', 'post_id', 'user_id');
+    return $this->belongsToMany(User::class, 'shared_posts', 'post_id', 'user_id');
   }
 }
